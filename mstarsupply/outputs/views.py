954e5e78e7c9bd30d80379714b202f9faf3d5700
giovani.alves <giovani.alves.glv@gmail.com>
2022-07-27 14:13:59 -0300

    Adicionada verificação de disponibilidade de mercadoria no método POST no módulo de saídas

--- mstarsupply/outputs/views.py
+++ mstarsupply/outputs/views.py
@@ -1,5 +1,8 @@
 from outputs.models import Output
+from inputs.models import Input
 from outputs.serializers import OutputSerializer
+from inputs.serializers import InputSerializer
+from django.db.models import Sum
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -7,16 +10,27 @@ from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def outputs_manager(request):
+
    if request.method == 'GET':
       outputs = Output.objects.all()
       serializer = OutputSerializer(outputs, many=True)
       return Response(serializer.data)
+
    elif request.method == 'POST':
-      serializer = OutputSerializer(data=request.data)
-      if serializer.is_valid():
-         serializer.save(merchandise_id=request.data['merchandise'])
-         return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      # verificando a disponibilidade da mercadoria
+      inputs = Input.objects.filter(merchandise_id=request.data['merchandise']).aggregate(Sum('qtd_goods'))
+      outputs = Output.objects.filter(merchandise_id=request.data['merchandise']).aggregate(Sum('qtd_goods'))
+      
+      # Se não tiver valor nenhum assume o valor zero '0'
+      stock = (inputs['qtd_goods__sum'] or 0) - (outputs['qtd_goods__sum'] or 0)
+
+      if request.data['qtd_goods'] <= stock:
+         serializer = OutputSerializer(data=request.data)
+         if serializer.is_valid():
+            serializer.save(merchandise_id=request.data['merchandise'])
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      return Response(f"No momento possuímos apenas {stock} unidade(s) dessa mercadoria!", status=status.HTTP_403_FORBIDDEN)
 
 @api_view(['GET', 'PUT'])
 def outputs_manager_detail_and_change(request, pk):
